711f0c7a6f91024716939c8a7bd6f853fdc6bbc5
Erik Donato <erikdonato@gmail.com>
2026-05-01 14:34:15 -0600

    copied package-lock from actions-md-book

--- __tests__/get-url.test.ts
+++ __tests__/get-url.test.ts
@@ -3,15 +3,15 @@ import { describe, expect, test } from '@jest/globals';
 
 describe('getURL()', () => {
   test('return extected URL', () => {
-    const testVersion: string = '0.3.5';
-    const baseURL: string = `https://github.com/voidmerge/voidmerge/releases/download/v${testVersion}/voidmerge-v${testVersion}`;
+    const testVersion: string = 'v0.3.5';
+    const baseURL: string = `https://github.com/voidmerge/voidmerge/releases/download/${testVersion}/voidmerge-${testVersion}`;
     const urlLinux: string = `${baseURL}-x86_64-unknown-linux-gnu.tar.gz`;
     const urlMacOS: string = `${baseURL}-aarch64-apple-darwin.tar.gz`;
     const urlWindows: string = `${baseURL}-x86_64-pc-windows-msvc.zip`;
-    expect(getURL('unknown-linux-gnu', '0.3.5')).toBe(urlLinux);
-    expect(getURL('unknown-linux-gnu', '0.3.4')).not.toBe(urlLinux);
-    expect(getURL('my-os', '0.3.5')).not.toBe(urlLinux);
-    expect(getURL('apple-darwin', '0.3.5')).toBe(urlMacOS);
-    expect(getURL('pc-windows-msvc', '0.3.5')).toBe(urlWindows);
+    expect(getURL('unknown-linux-gnu', 'v0.3.5')).toBe(urlLinux);
+    expect(getURL('unknown-linux-gnu', 'v0.3.4')).not.toBe(urlLinux);
+    expect(getURL('my-os', 'v0.3.5')).not.toBe(urlLinux);
+    expect(getURL('apple-darwin', 'v0.3.5')).toBe(urlMacOS);
+    expect(getURL('pc-windows-msvc', 'v0.3.5')).toBe(urlWindows);
   });
 });
