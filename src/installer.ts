4198678e4f5b49b4fd11288e22f18e6e8babd01c
Erik Donato <erikdonato@gmail.com>
2026-05-01 14:40:22 -0600

    undo tmp directory in installer

--- src/installer.ts
+++ src/installer.ts
@@ -23,7 +23,7 @@ export async function createTempDir(baseLocation: string): Promise<string> {
   let tempDir: string = process.env['RUNNER_TEMPDIRECTORY'] || '';
 
   if (tempDir === '') {
-    tempDir = path.join(baseLocation, 'toolbin');
+    tempDir = path.join(baseLocation, 'tmp');
   }
 
   await io.mkdirP(tempDir);
